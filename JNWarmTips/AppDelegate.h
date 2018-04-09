54942987446c3e4b853703330d37fcc734b0acf0
冯天宇 <fengtianyu@syswin.com>
2018-04-09 11:12:25 +0800

    Initial Commit

---
+++ JNWarmTips/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JNWarmTips
+//
+//  Created by fengtianyu on 9/4/18.
+//  Copyright © 2018年 fengtianyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JNWarmTips/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JNWarmTips
+//
+//  Created by fengtianyu on 9/4/18.
+//  Copyright © 2018年 fengtianyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
