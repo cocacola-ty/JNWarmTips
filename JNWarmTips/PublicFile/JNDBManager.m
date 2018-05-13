b854acc838771076a5ccfe5099c5c039b6f6f984
fengtianyu <fengtianyu@syswin.com>
2018-05-13 15:11:05 +0800

    更新数据表结构

--- JNWarmTips/PublicFile/JNDBManager.m
+++ JNWarmTips/PublicFile/JNDBManager.m
@@ -6,6 +6,22 @@
 #import "JNDBManager.h"
 #import <fmdb/FMDB.h>
 
+/*
+ * 事件表和清单表的字段为
+    id 
+    内容 
+    show_date ：该事件的日期。清单中无该字段。清单向日历同步时该字段为清单的start_time中的日期
+    开始时间 ：时间戳。用于显示时间段
+    结束时间 ：时间戳
+    标签id ：
+    分组id ：
+    是否完成：默认为0
+    是否需要通知：默认为0
+ 
+ * 分组表
+    分组名不可重复
+ */
+
 static NSString *const kJNDBListTable = @"list_table";
 
 static NSString *const kJNDBEventsTable = @"events_table";
@@ -21,7 +37,7 @@ static NSString *const kJNDBCategoryTable = @"category_table";
 + (instancetype) shareInstance {
     static JNDBManager *instance = nil;
     static dispatch_once_t onceToken;
-    dispatch_once(onceToken, ^{
+    dispatch_once(&onceToken, ^{
         instance = [[self alloc] init];
     });
     return instance;
@@ -43,46 +59,43 @@ static NSString *const kJNDBCategoryTable = @"category_table";
 }
 
 - (void) createTables {
-
-    if (![self tableExist:kJNDBListTable]) {
-        [self.dbQueue inTransaction:^(FMDatabase *db, BOOL *rollback) {
-            NSString *sql = [NSString stringWithFormat:@"CREATE TABLE IF NOT EXIST %@ (ITEM_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, CONTENT TEXT DEFAULT '', DATE TEXT DEFAULT '', TIME TEXT DEFAULT '', TAG TEXT DEFAULT '', CATEGORY TEXT DEFAULT '', NOTIFICATION INTEGER DEFAULT 0, FINISHED INTEGER DEFAULT 0 )", kJNDBListTable];
-            BOOL result = [db executeUpdate:sql];
-            if (!result) {
-                NSLog(@"list表创建失败");
-            }
-        }];
-    }
-
-    if (![self tableExist:kJNDBEventsTable]) {
-        [self.dbQueue inTransaction:^(FMDatabase *db, BOOL *rollback) {
-            NSString *sql = [NSString stringWithFormat:@"CREATE TABLE IF NOT EXIST %@ (EVENT_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, CONTENT TEXT DEFAULT '', DATE TEXT DEFAULT '', TIME TEXT DEFAULT '', TAG TEXT DEFAULT '', CATEGORY TEXT DEFAULT '', NOTIFICATION INTEGER DEFAULT 0, FINISHED INTEGER DEFAULT 0 )", kJNDBEventsTable];
-            BOOL result = [db executeUpdate:sql];
-            if (!result) {
-                NSLog(@"list表创建失败");
-            }
-        }];
-    }
-
+    
+    // 小组表
     if (![self tableExist:kJNDBGroupTable]) {
         [self.dbQueue inTransaction:^(FMDatabase *db, BOOL *rollback) {
-            NSString *sql = [NSString stringWithFormat:@"CREATE TABLE IF NOT EXIST %@ (GROUP_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, GROUP_NAME TEXT DEFAULT '')", kJNDBGroupTable];
+            NSString *sql = [NSString stringWithFormat:@"CREATE TABLE IF NOT EXIST %@ (GROUP_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, GROUP_NAME TEXT NOT NULL)", kJNDBGroupTable];
             BOOL result = [db executeUpdate:sql];
-            if (!result) {
-                NSLog(@"list表创建失败");
-            }
+            NSAssert(!result, @"小组表创建失败");
+        }];
+    }
+    
+    // 分类表
+    if (![self tableExist:kJNDBCategoryTable]) {
+        [self.dbQueue inTransaction:^(FMDatabase *db, BOOL *rollback) {
+            NSString *sql = [NSString stringWithFormat:@"CREATE TABLE IF NOT EXIST %@ (CATEGORY_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, CATEGORY_NAME TEXT NOT NULL , GROUP_ID INTEGER, FOREIGN KEY(GROUP_ID) REFERENCES %@(GROUP_ID))", kJNDBCategoryTable, kJNDBGroupTable];
+            BOOL result = [db executeUpdate:sql];
+             NSAssert(!result, @"分类表创建失败");
         }];
     }
 
-    if (![self tableExist:kJNDBCategoryTable]) {
+    // 清单表
+    if (![self tableExist:kJNDBListTable]) {
         [self.dbQueue inTransaction:^(FMDatabase *db, BOOL *rollback) {
-            NSString *sql = [NSString stringWithFormat:@"CREATE TABLE IF NOT EXIST %@ (CATEGORY_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, CATEGORY_NAME TEXT DEFAULT '', FOREIGN KEY(GROUP_ID) REFERENCES %@(GROUP_ID))", kJNDBCategoryTable, kJNDBGroupTable];
+            NSString *sql = [NSString stringWithFormat:@"CREATE TABLE IF NOT EXIST %@ (ITEM_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, CONTENT TEXT NOT NULL, START_TIME INTEGER DEFAULT NULL, END_TIME INTEGER DEFAULT NULL, TAG_ID INTEGER, CATEGORY_ID INTEGER , NOTIFICATION INTEGER DEFAULT 0, FINISHED INTEGER DEFAULT 0, FOREIGN KEY(GROUP_ID) REFERENCES %@(GROUP_ID), FOREIGN KEY(CATEGORY_ID) REFERENCES %@(CATEGORY_ID))", kJNDBListTable, kJNDBGroupTable, kJNDBCategoryTable];
             BOOL result = [db executeUpdate:sql];
-            if (!result) {
-                NSLog(@"list表创建失败");
-            }
+            NSAssert(!result, @"清单表创建失败");
         }];
     }
+
+    // 事件表
+    if (![self tableExist:kJNDBEventsTable]) {
+        [self.dbQueue inTransaction:^(FMDatabase *db, BOOL *rollback) {
+            NSString *sql = [NSString stringWithFormat:@"CREATE TABLE IF NOT EXIST %@ (EVENT_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, CONTENT TEXT NOT NULL, SHOW_DATE DATE NOT NULL, START_TIME INTEGER DEFAULT NULL, END_TIME INTEGER DEFAULT NULL, TAG_ID INTEGER, CATEGORY_ID INTEGER , NOTIFICATION INTEGER DEFAULT 0, FINISHED INTEGER DEFAULT 0, FOREIGN KEY(GROUP_ID) REFERENCES %@(GROUP_ID), FOREIGN KEY(CATEGORY_ID) REFERENCES %@(CATEGORY_ID))", kJNDBListTable, kJNDBGroupTable, kJNDBCategoryTable];
+            BOOL result = [db executeUpdate:sql];
+            NSAssert(!result, @"事件表创建失败");
+        }];
+    }
+
 }
 
 - (BOOL) tableExist:(NSString *)tableName {
