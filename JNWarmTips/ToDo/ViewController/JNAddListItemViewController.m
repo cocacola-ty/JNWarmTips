b22bd27bc5cb2b444acde6f3c69af52094e4c656
fengtianyu <fengtianyu@syswin.com>
2018-08-19 21:29:57 +0800

    更新添加事项页面的进出动画

--- JNWarmTips/ToDo/ViewController/JNAddListItemViewController.m
+++ JNWarmTips/ToDo/ViewController/JNAddListItemViewController.m
@@ -22,18 +22,47 @@
     self.view.backgroundColor = [UIColor colorWithRed:211/255.0 green:211/255.0 blue:211/255.0 alpha:0.6];
     
     [self.view addSubview:self.containerView];
-    CGFloat topMargin = SCREEN_HEIGHT / 2 - 100;
+    
     [self.containerView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.view.mas_left);
         make.right.equalTo(self.view.mas_right);
         make.bottom.equalTo(self.view.mas_bottom);
-        make.top.equalTo(self.view.mas_top).offset(topMargin);
+        make.top.equalTo(self.view.mas_top).offset(SCREEN_HEIGHT);
+//        make.top.equalTo(self.view.mas_bottom);
     }];
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    CGFloat topMargin = SCREEN_HEIGHT / 2 - 100;
+    [self.containerView mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view.mas_top).offset(topMargin);
+    }];
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        [self.view layoutIfNeeded];
+        [self.view setNeedsLayout];
+    } completion:^(BOOL finished) {
+        // 成为第一响应者
+    }];
+}
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [self dismissViewControllerAnimated:NO completion:nil];
+    [self dismiss];
+}
+
+- (void) dismiss {
+    
+    [self.containerView mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view.mas_top).offset(SCREEN_HEIGHT);
+    }];
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        [self.view layoutIfNeeded];
+        [self.view setNeedsLayout];
+    } completion:^(BOOL finished) {
+        [self dismissViewControllerAnimated:NO completion:nil];
+    }];
 }
 
 #pragma mark - Getter & Setter
