ec9ae534df103dc30a3ef85fe714c6b9b37f3239
fengtianyu <fengtianyu@syswin.com>
2018-10-31 18:38:21 +0800

    优化图片的加载内存

--- JNWarmTips/ToDo/View/JNItemGroupCell.m
+++ JNWarmTips/ToDo/View/JNItemGroupCell.m
@@ -18,6 +18,8 @@ static NSString * const kAnimationKey = @"shakeAnimation";
 @property (nonatomic, strong) UIView *shadowView;
 @property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic, strong) UILabel *groupNameLabel;
+@property (nonatomic, strong) UIButton *deleteBtn;
+@property (nonatomic, strong) UIImage *img;
 
 @property(nonatomic, assign) CGFloat containerViewWidth;
 @property(nonatomic, assign) CGFloat containerViewHeight;
@@ -31,6 +33,8 @@ static NSString * const kAnimationKey = @"shakeAnimation";
 
     if (self) {
 
+        self.backgroundColor = [UIColor whiteColor];
+        
         [self.contentView addSubview:self.shadowView];
         [self.shadowView mas_makeConstraints:^(MASConstraintMaker *make) {
             make.top.equalTo(self.contentView.mas_top).offset(kDefaultMargin);
@@ -60,9 +64,45 @@ static NSString * const kAnimationKey = @"shakeAnimation";
             make.centerX.equalTo(self.containerView.mas_centerX);
             make.top.equalTo(self.imageView.mas_bottom).offset(10);
         }];
+        
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            UIImage *img = [self handleImage];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.imageView.image = img;
+            });
+        });
     }
     return self;
 }
+
+#pragma mark - Private Method
+
+- (UIImage *)handleImage {
+    
+    int num = arc4random() % 8 + 1;
+    NSString *imageName = [NSString stringWithFormat:@"group_bg%d.jpg", num];
+    NSString *path = [[NSBundle mainBundle] pathForResource:imageName ofType:nil];
+    UIImage *sourceImg = [UIImage imageWithContentsOfFile:path];
+    NSLog(@"source width - %f, source height - %f", sourceImg.size.width, sourceImg.size.height);
+    
+    CGImageRef sourceImage = sourceImg.CGImage;
+    
+    size_t bits = CGImageGetBitsPerComponent(sourceImage);
+    size_t bytesPerRow = CGImageGetBytesPerRow(sourceImage);
+    CGColorSpaceRef spaceRef = CGImageGetColorSpace(sourceImage);
+    uint32_t bitmapInfo = CGImageGetBitmapInfo(sourceImage);
+    
+    CGContextRef ctx = CGBitmapContextCreate(NULL, 270, 280, bits, bytesPerRow, spaceRef, bitmapInfo);
+    CGContextSetInterpolationQuality(ctx, kCGInterpolationDefault);
+    CGContextDrawImage(ctx, CGRectMake(0, 0, 270, 280), sourceImage);
+    
+    CGImageRef desImage = CGBitmapContextCreateImage(ctx);
+    
+    UIImage *resImage = [UIImage imageWithCGImage:desImage];
+    return resImage;
+}
+
+#pragma mark - Public Method
 
 - (void)startShake {
     
@@ -115,15 +155,15 @@ static NSString * const kAnimationKey = @"shakeAnimation";
     if (!_imageView) {
         _imageView = [UIImageView new];
         _imageView.contentMode = UIViewContentModeScaleToFill;
-        dispatch_async(dispatch_get_global_queue(0, 0), ^{
-            int num = arc4random() % 8 + 1;
-            NSString *imageName = [NSString stringWithFormat:@"group_bg%d.jpg", num];
-            NSLog(@"%@", imageName);
-            UIImage *image = [UIImage imageNamed:imageName];
-            dispatch_async(dispatch_get_main_queue(), ^{
-                _imageView.image = image;
-            });
-        });
+//        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+//            int num = arc4random() % 8 + 1;
+//            NSString *imageName = [NSString stringWithFormat:@"group_bg%d.jpg", num];
+//            NSLog(@"%@", imageName);
+//            UIImage *image = [UIImage imageNamed:imageName];
+//            dispatch_async(dispatch_get_main_queue(), ^{
+//                _imageView.image = image;
+//            });
+//        });
     }
     return _imageView;
 }
@@ -131,9 +171,19 @@ static NSString * const kAnimationKey = @"shakeAnimation";
 - (UILabel *)groupNameLabel {
     if (!_groupNameLabel) {
         _groupNameLabel = [UILabel new];
+        _groupNameLabel.backgroundColor = [UIColor whiteColor];
         _groupNameLabel.font = [UIFont boldSystemFontOfSize:14.0];
     }
     return _groupNameLabel;
+}
+
+- (UIButton *)deleteBtn {
+    if (!_deleteBtn) {
+        _deleteBtn = [[UIButton alloc] init];
+        [_deleteBtn setBackgroundImage:[UIImage imageNamed:@"error"] forState:UIControlStateNormal];
+        _deleteBtn.backgroundColor = [UIColor whiteColor];
+    }
+    return _deleteBtn;
 }
 
 - (void)setGroupModel:(JNGroupModel *)groupModel {
