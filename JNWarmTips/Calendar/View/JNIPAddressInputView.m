05cb8df31bad2d92eab0d0ee6432b2c6e6e29235
fengtianyu <fengtianyu@syswin.com>
2018-12-06 10:58:41 +0800

    键盘添加上一段下一段移动及事件响应

--- JNWarmTips/Calendar/View/JNIPAddressInputView.m
+++ JNWarmTips/Calendar/View/JNIPAddressInputView.m
@@ -18,6 +18,11 @@
 @property (nonatomic, strong) UILabel *ipFourthLabel;
 
 @property (nonatomic, strong) NSArray *ipLabelArray;
+
+@property (nonatomic, strong) UIButton *preBtn;
+@property (nonatomic, strong) UIButton *nextBtn;
+
+@property (nonatomic, assign) NSInteger focusIndex;
 @end
 
 @implementation JNIPAddressInputView
@@ -25,6 +30,8 @@
 - (instancetype)init {
     self = [super init];
     if (self) {
+        self.focusIndex = 1;
+        
         [self addSubview:self.textField];
         [self addSubview:self.ipFirstLabel];
         [self addSubview:self.ipSecondLabel];
@@ -33,22 +40,9 @@
         
         self.ipLabelArray = @[self.ipFirstLabel, self.ipSecondLabel, self.ipThirdLabel, self.ipFourthLabel];
         [self addDotLayer];
+        [self addFocusLineLayer];
         
-        CALayer *lineLayer = [CALayer layer];
-        lineLayer.frame = CGRectMake(50, 10, 3, 30);
-        lineLayer.backgroundColor = MAIN_COLOR.CGColor;
-        [self.layer addSublayer:lineLayer];
-        
-        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"opacity"];//必须写opacity才行。
-        animation.fromValue = [NSNumber numberWithFloat:1.0f];
-        animation.toValue = [NSNumber numberWithFloat:0.0f];//这是透明度。
-        animation.autoreverses = YES;
-        animation.duration = 0.25;
-        animation.repeatCount = MAXFLOAT;
-        animation.removedOnCompletion = NO;
-        animation.fillMode = kCAFillModeForwards;
-        animation.timingFunction=[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];///没有的话是均匀的动画。
-        [lineLayer addAnimation:animation forKey:nil];
+        [self addObserver:self forKeyPath:@"focusIndex" options:NSKeyValueObservingOptionNew context:nil];
     }
     return self;
 }
@@ -82,6 +76,24 @@
     }
 }
 
+- (void)addFocusLineLayer {
+    CALayer *lineLayer = [CALayer layer];
+    lineLayer.frame = CGRectMake(50, 10, 3, 30);
+    lineLayer.backgroundColor = MAIN_COLOR.CGColor;
+    [self.layer addSublayer:lineLayer];
+    
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"opacity"];//必须写opacity才行。
+    animation.fromValue = [NSNumber numberWithFloat:1.0f];
+    animation.toValue = [NSNumber numberWithFloat:0.0f];//这是透明度。
+    animation.autoreverses = YES;
+    animation.duration = 0.35;
+    animation.repeatCount = MAXFLOAT;
+    animation.removedOnCompletion = NO;
+    animation.fillMode = kCAFillModeForwards;
+    animation.timingFunction=[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];///没有的话是均匀的动画。
+    [lineLayer addAnimation:animation forKey:nil];
+}
+
 - (void)labelSetting:(UILabel *)lbl {
     lbl.text = @"192";
     lbl.font = [UIFont boldSystemFontOfSize:18.0];
@@ -94,11 +106,28 @@
     [self.textField becomeFirstResponder];
 }
 
+#pragma mark - Event Response
+
+- (void)moveFocusToNext {
+    NSLog(@"next ");
+    self.focusIndex = self.focusIndex + 1;
+}
+
+- (void)moveFocusToPre {
+    NSLog(@"pre ");
+    self.focusIndex = self.focusIndex - 1;
+}
 #pragma mark - Delegate
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
     
     return YES;
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    NSInteger positionIndex = [[change valueForKey:NSKeyValueChangeNewKey] integerValue];
+    self.preBtn.enabled = positionIndex > 1;
+    self.nextBtn.enabled = positionIndex < 4;
 }
 
 #pragma mark - Getter
@@ -108,10 +137,44 @@
     if (!_textField) {
         _textField = [[UITextField alloc] init];
         _textField.delegate = self;
-        _textField.keyboardType = UIKeyboardTypePhonePad;
+        _textField.keyboardType = UIKeyboardTypeNumberPad;
         _textField.returnKeyType = UIReturnKeyNext;
+        
+        _textField.inputAccessoryView = [self keyBoardToolBar];
     }
     return _textField;
+}
+
+- (UIView *)keyBoardToolBar {
+    
+    CGFloat toolBarHeight = 44;
+    CGFloat btnWidth = 70;
+    
+    UIView *toolBar = [UIView new];
+    toolBar.frame = CGRectMake(0, 0, SCREEN_WIDTH, toolBarHeight);
+    toolBar.backgroundColor = [UIColor whiteColor];
+    
+    UIButton *nextBtn = [[UIButton alloc] init];
+    [nextBtn setTitle:@"下一段" forState:UIControlStateNormal];
+    nextBtn.frame = CGRectMake(SCREEN_WIDTH - btnWidth, 0, btnWidth, toolBarHeight);
+    [nextBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [nextBtn setTitleColor:[UIColor grayColor] forState:UIControlStateDisabled];
+    nextBtn.titleLabel.font = [UIFont boldSystemFontOfSize:15.0];
+    [nextBtn addTarget:self action:@selector(moveFocusToNext) forControlEvents:UIControlEventTouchUpInside];
+    [toolBar addSubview:nextBtn];
+    self.nextBtn = nextBtn;
+    
+    UIButton *preBtn = [UIButton new];
+    preBtn.frame = CGRectMake(0, 0, btnWidth, toolBarHeight);
+    [preBtn setTitle:@"上一段" forState:UIControlStateNormal];
+    [preBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [preBtn setTitleColor:[UIColor grayColor] forState:UIControlStateDisabled];
+    preBtn.titleLabel.font = [UIFont boldSystemFontOfSize:15.0];
+    [preBtn addTarget:self action:@selector(moveFocusToPre) forControlEvents:UIControlEventTouchUpInside];
+    preBtn.enabled = NO;
+    [toolBar addSubview:preBtn];
+    self.preBtn = preBtn;
+    return toolBar;
 }
 
 - (UILabel *)ipFirstLabel {
